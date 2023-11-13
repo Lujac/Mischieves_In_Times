5401cc385042472699f174191f2d984c1295aff0
Ewen-Beeprof <ewen.celibert@beeprof.fr>
2023-11-13 17:29:37 +0100

    Locked Dico before Sherlock dialog

--- Assets/Scripts/Tablette/tab_Dico.cs
+++ Assets/Scripts/Tablette/tab_Dico.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class tab_Dico : MonoBehaviour
 {
     CharacterSherlockBehavior SherlockState;
+    GameObject PuzzleDico;
 
     void OnEnable()
     {
         if(!SherlockState) SherlockState = GameObject.Find("PNJ_Sherlock").GetComponent<CharacterSherlockBehavior>();
+        if(!PuzzleDico) PuzzleDico = this.gameObject.transform.GetChild(0).Find("PuzzleDico").gameObject;
+
+        if(SherlockState.DialogueSherlockGreetCompleted && !PuzzleDico.activeSelf) PuzzleDico.SetActive(true);
         
         // Debug.Log(SherlockState.DialogueSherlockGreetCompleted);
     }

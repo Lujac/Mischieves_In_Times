52adcec76f74451d50ce5063bab66d128a9334c0
Ewen-Beeprof <144323923+Ewen-Beeprof@users.noreply.github.com>
2023-12-14 11:53:36 +0100

    Added Sherlock Block & Dico popup

--- Assets/Scripts/Tablette/tab_Dico.cs
+++ Assets/Scripts/Tablette/tab_Dico.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class tab_Dico : MonoBehaviour
 {
     CharacterSherlockBehavior SherlockState;
+    GameObject PopupDico;
     GameObject PuzzleDico;
 
     void OnEnable()
     {
         if(!SherlockState) SherlockState = GameObject.Find("PNJ_Sherlock").GetComponent<CharacterSherlockBehavior>();
+        if(!PopupDico) PopupDico = this.gameObject.transform.GetChild(0).Find("PopupDico").gameObject;
         if(!PuzzleDico) PuzzleDico = this.gameObject.transform.GetChild(0).Find("PuzzleDico").gameObject;
 
+        if(SherlockState.DialogueSherlockGreetCompleted && !PopupDico.activeSelf) PopupDico.SetActive(true);
         if(SherlockState.DialogueSherlockGreetCompleted && !PuzzleDico.activeSelf) PuzzleDico.SetActive(true);
         
         // Debug.Log(SherlockState.DialogueSherlockGreetCompleted);
